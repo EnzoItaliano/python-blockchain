f3ab12fca7e6fb623d6f377881e9c4e251329932
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2022-12-11 16:20:13 +0000

    refactor: removing unnecessary owner's code

--- src/common/owner.py
+++ src/common/owner.py
@@ -1,8 +1,4 @@
-import binascii
-
 from Crypto.PublicKey import RSA
-
-from ..common.utils import calculate_hash
 
 
 class Owner:
@@ -12,8 +8,3 @@ class Owner:
         else:
             self.private_key = RSA.generate(2048)
         self.public_key = self.private_key.publickey().export_key()
-        # self.public_key_hex = binascii.hexlify(public_key).decode("utf-8")
-        # self.public_key_hash = calculate_hash(
-        #     calculate_hash(self.public_key_hex, hash_function="sha256"),
-        #     hash_function="ripemd160",
-        # )
